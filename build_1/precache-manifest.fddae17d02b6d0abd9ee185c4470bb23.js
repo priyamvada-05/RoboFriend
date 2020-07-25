0ec2f0f4ec534cf5434a93c9a062ae0955df6e7b
Rutika  Ostwal <rutikaostwal@Rutikas-MacBook-Air.local>
2020-07-25 01:14:55 +0100

    adding

---
+++ build_1/precache-manifest.fddae17d02b6d0abd9ee185c4470bb23.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebc91957b3ecb0a0941374592951b6a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61f69a7b0b1d1618ce01",
+    "url": "/static/css/2.506b6d60.chunk.css"
+  },
+  {
+    "revision": "769081805f3b359bdd0c",
+    "url": "/static/css/main.ba41daa2.chunk.css"
+  },
+  {
+    "revision": "61f69a7b0b1d1618ce01",
+    "url": "/static/js/2.5a9413cf.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.5a9413cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "769081805f3b359bdd0c",
+    "url": "/static/js/main.c310d7c1.chunk.js"
+  },
+  {
+    "revision": "0ae1a9d39e01f4ca23fa",
+    "url": "/static/js/runtime-main.eb99bd7f.js"
+  },
+  {
+    "revision": "b7ddd2037689fbea641a1b6f4df3dcd8",
+    "url": "/static/media/coming_soon.b7ddd203.jpg"
+  },
+  {
+    "revision": "56b805cbc105f9c85e368dc670a4533d",
+    "url": "/static/media/coming_soon_text.56b805cb.jpg"
+  },
+  {
+    "revision": "b74f36bb44779e651fc92cdef2f8b8d2",
+    "url": "/static/media/logo8.b74f36bb.jpeg"
+  },
+  {
+    "revision": "4a9c909d75a780386c29606add04266e",
+    "url": "/static/media/subscribe_today_text.4a9c909d.jpg"
+  }
+]);
